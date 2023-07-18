5c1563db39357245cca51ff809dd84aeb17e27a1
Jason Wells <jewells1980@gmail.com>
2023-07-18 13:59:38 -0500

    fnm changes and shortcut changes

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,3 +1,5 @@
 # fnm
-set PATH "/home/$USER/.local/share/fnm" $PATH
-fnm env --use-on-cd | source
+if [ "$TERM_PROGRAM" != "vscode" ]
+	set PATH "/home/$USER/.local/share/fnm" $PATH
+	fnm env --use-on-cd | source
+end
